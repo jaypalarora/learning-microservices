fc4edbcd5ec208e5aac901f18715032ddbad2071
jaypalarora <jaypalarora@gmail.com>
2023-07-06 13:30:29 +0530

    App Discovery Service using Eureka

---
+++ AppDiscoveryService/src/test/java/com/app/appdiscoveryservice/AppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.appdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
